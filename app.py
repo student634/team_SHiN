4063bc807274558e329a8e5e878f60b620cd59c6
student634 <117338252+student634@users.noreply.github.com>
2023-03-12 21:49:14 +0900

    unsolved.htmlを微修正

--- app.py
+++ app.py
@@ -271,10 +271,9 @@ def display_solved():
 @app.route("/edit/<path:error_id>", methods=["GET", "POST"])
 @login_required
 def edit(error_id):
+    # record内容を変更する場合
     if request.method == "POST":
 
-        # エラー言語
-        # language = request.form.get("language")
         # エラー文
         error = request.form.get("error")
         # 状況説明
@@ -284,10 +283,13 @@ def edit(error_id):
         #解決か未解決か
         public = request.form.get("status")
 
+        # エラー文、状況説明、解決策、解決or未解決の内容を更新し、更新日時を変更
         db.execute("UPDATE errors SET message = ?, explain = ?, solved = ?, public = ?, after_day = DATETIME('now','localtime') WHERE error_id = ?", error, explanation, solution, public, error_id)
         return render_template("solved.html")
 
+    # 編集画面に飛ぶ場合
     else :
+        # solved.htmlの52行目からerror_idを取得し、該当するデータベースの内容を抜き出す
         edit_errors = db.execute("SELECT * FROM errors WHERE error_id = ?", error_id)
         return render_template("edit.html", language=LANGUAGES, edit_errors=edit_errors[0])
 
@@ -295,7 +297,7 @@ def edit(error_id):
 
 # 共有画面の表示
 @app.route("/timeline", methods=["GET", "POST"])
-@login.required
+@login_required
 def timeline():
 
     # 検索したい場合
@@ -304,15 +306,15 @@ def timeline():
         # どの言語で絞るか form から受け取る
         language = request.form.get("language")
 
-            if language == "すべての言語":
-                # すべての解決済みをデータベースから取り出し、格納
-                solved_errors = db.execute("SELECT * FROM errors WHERE public LIKE '解決'")
+        if language == "すべての言語":
+            # すべての解決済みをデータベースから取り出し、格納
+            solved_errors = db.execute("SELECT * FROM errors WHERE public LIKE '解決'")
 
-            else:
-                # 特定の言語の解決済エラーをデータベースから取り出し、格納
-                solved_errors = db.execute("SELECT * FROM errors WHERE public LIKE '解決' AND language=?", language)
+        else:
+            # 特定の言語の解決済エラーをデータベースから取り出し、格納
+            solved_errors = db.execute("SELECT * FROM errors WHERE public LIKE '解決' AND language=?", language)
 
-            return render_template("timeline.html", solved_errors=solved_errors, languages=LANGUAGES)
+        return render_template("timeline.html", solved_errors=solved_errors, languages=LANGUAGES)
 
     # 解決済みを並べる
     else:
